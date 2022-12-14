b556fb24ad8720e66958c17076496dc5ddf69ee6
Paulcollins <pcollinsmb@gmail.com>
2022-12-14 13:34:38 +0100

    suppress mongoose strictQuery warning

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 const connectDB = async () => {
-  mongoose.set("toJSON", { virtuals: true });  
+  mongoose.set("toJSON", { virtuals: true });
+  mongoose.set("strictQuery", true);
   mongoose.set("toObject", { virtuals: true });             
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     autoIndex: true,
